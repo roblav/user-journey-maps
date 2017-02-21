c12aaacc2fbd8e0e227148058fa5aecd889533d2
robert.lavender <robert.lavender@digital.hmrc.gov.uk>
2017-02-21 14:19:14 +0000

    Add functionality to handle dynamic port assignmnet.

--- app/routes.js
+++ app/routes.js
@@ -4,9 +4,11 @@ var app = express();
 var path = require('path');
 var open = require('open');
 var browserSync = require('browser-sync');
+var config = require('./config.js')
+var utils = require('../lib/utils.js')
 
-var port = process.env.PORT || (process.argv[2] || 3002);
-port = (typeof port === "number") ? port : 3002;
+//var port = process.env.PORT || (process.argv[2] || 3000);
+//port = (typeof port === "number") ? port : 3000;
 
 nunjucks.configure(path.join(__dirname, '/views/'), {
   autoescape: true,
@@ -19,17 +21,19 @@ app.get('/', function(req, res) {
   res.render('index.html', {title : 'My First Nunjucks Page'});
 });
 
-app.listen(port, function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    open('http://localhost:' + port);
-  }
-  browserSync({
-    proxy: 'localhost:' + (port),
-    port: port,
-    ui: false,
-    files: ["app/views/*.html"]
+utils.findAvailablePort(app, function (port) {
+  app.listen(port, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      open('http://localhost:' + port);
+    }
+    browserSync({
+      proxy: 'localhost:' + (port),
+      port: port,
+      ui: false,
+      files: ["app/views/*.html"]
+    });
   });
 });
 
