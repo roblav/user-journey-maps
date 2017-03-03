2ae132c4605949181e93b807ad2ca0a8358cec18
robert.lavender <robert.lavender@digital.hmrc.gov.uk>
2017-03-03 11:59:22 +0000

    Hook up checkboxes with unslider display.

--- app/routes.js
+++ app/routes.js
@@ -7,6 +7,9 @@ var browserSync = require('browser-sync');
 var config = require('./config.js');
 var utils = require('../lib/utils.js');
 
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
 // Middleware to serve static assets
 app.use('/public', express.static('public'))
 
@@ -34,11 +37,22 @@ nunjucks.configure(path.join(__dirname, '/views/'), {
 var dirPath = 'public/images'
 
 var imgFiles = [
-  'overpaid-issued-available-1.png',
-  'overpaid-issued-available-2.png',
-  'overpaid-issued-processing-1.png',
+  'underpaid-issued-outstanding-4.png',
+  'underpaid-issued-outstanding-3.png',
+  'underpaid-issued-outstanding-2.png',
+  'underpaid-issued-outstanding-1.png',
+  'underpaid-issued-coded-out-2.png',
   'underpaid-issued-coded-out-1.png',
-  'underpaid-issued-coded-out-2.png'
+  'overpaid-issued-processing-2.png',
+  'overpaid-issued-processing-1.png',
+  'overpaid-issued-not-available-2.png',
+  'overpaid-issued-not-available-1.png',
+  'overpaid-issued-claimed-2.png',
+  'overpaid-issued-claimed-1.png',
+  'overpaid-issued-cheque-sent-2.png',
+  'overpaid-issued-cheque-sent-1.png',
+  'overpaid-issued-available-2.png',
+  'overpaid-issued-available-1.png'
   ]
 
 
@@ -59,19 +73,60 @@ app.get('/impress-test', function(req, res) {
 });
 
 utils.findAvailablePort(app, function (port) {
-  app.listen(port, function(err) {
+  server.listen(port, function(err) {
     if (err) {
       console.log(err);
     } else {
       open('http://localhost:' + port);
     }
-    browserSync({
-      proxy: 'localhost:' + (port),
-      port: port,
-      ui: false,
-      files: ["app/views/*.html"]
-    });
+    //browserSync({
+    //  proxy: 'localhost:' + (port),
+    //  port: port,
+    //  ui: false,
+    //  files: ["app/views/*.html"]
+    //});
   });
 });
 
 
+
+io.on('connection', function(socket) {
+  //console.log('a user connected');
+  socket.on('disconnect', function () {
+    //console.log('user disconnected');
+  });
+
+  // Get user input for User Maps
+  // now return the correct manulUpdateMsg message
+  socket.on('checkboxOptions--usermaps', function(data) {
+    var imgSet = {}
+    //console.log(data)
+    // Use data to get set of images to pass back
+    // List out all of the values
+    for(var str in data) {
+      //console.log(str)
+      containsSubstring (imgFiles.reverse(), str)
+      //use prop value to find match in array
+    }
+    //compare substring against each array item
+    function containsSubstring (array, str) {
+      var strRegExp = new RegExp(str);
+      //console.log(str)
+      //for loop
+      for (var i=0; i < array.length; i++) {
+        //console.log(array[i] + " compare with " + str);
+
+        if (strRegExp.test(array[i])) {
+          //console.log(str + " matches - " + array[i]);
+          //Add to object
+          imgSet["img"+i] = array[i]
+        } else {
+          //console.log("Not a match");
+        }
+      }
+    }
+    //console.log(imgSet)
+    // Pass the data object back to the client
+    io.emit('checkboxOptions--usermaps-PUB', imgSet);
+  });
+});
---
+++ public/js/socket--img-set.js
@@ -0,0 +1,68 @@
+var socket = io();
+
+
+// ## User Map Selection
+// -----------------------
+
+$('#checkbox--usermaps input').change(function(){
+  var data = $('#checkbox--usermaps input:checked').serializeJSON();
+  // Only call manulUpdateMsg if something is checked
+  //if( radios.hasOwnProperty('manual') ) {
+  //  socket.emit('checkboxOptions--usermaps', dataMaps);
+  //}
+  //console.log(data)
+  socket.emit('checkboxOptions--usermaps', data);
+
+});
+
+
+// ## Display persona details
+// -----------------------------------------
+socket.on('checkboxOptions--usermaps-PUB', function(imgSet){
+  //console.log(imgSet)
+  //loop over obj to output values
+  var ul = document.getElementById("slider-list");
+  // Remove current slides
+  //console.log($('ul#slider-list li').length)
+  $('ul#slider-list li').remove();
+  //console.log($('ul#slider-list li').length)
+
+  for (var val in imgSet) {
+    // skip loop if the property is from prototype
+    if(!imgSet.hasOwnProperty(val)) continue;
+    //add to .my-slider as an li element
+    //console.log(imgSet[val])
+    var li = document.createElement("li");
+    var img = document.createElement("img");
+    img.setAttribute("src", "/public/images/"+imgSet[val]);
+    li.appendChild(img);
+    ul.appendChild(li);
+  }
+  //$('#userName').attr('value', userTes.userName);
+  // Let's recalculate Unslider so it knows what's going on
+  //window.slider = $('.my-slider').unslider({
+    //    animateHeight: true
+    //  })
+  //$('.my-slider').unslider('destroy')
+  //$('.my-slider').unslider({animateHeight: true})
+  //$('.my-slider').unslider();
+  slider('.my-slider');
+  //Move nav elements to the top
+  $('.my-slider ~ a, .my-slider ~ nav').insertBefore( ".my-slider" );
+});
+
+/* Unslider */
+function slider(divClass) {
+
+  // destroy if exists first
+  if ($(divClass).hasClass('unslider-horizontal')){
+    $(divClass).unwrap();
+    $(divClass).removeClass('unslider-horizontal');
+    $('.unslider-nav, .unslider-arrow').remove();
+  }
+
+  // init the slider
+  $(divClass).unslider({
+    animateHeight   : true
+  });
+}
