ede7a79236a9f039473d0fb795a25826caa2402b
robert.lavender <robert.lavender@digital.hmrc.gov.uk>
2017-03-16 13:58:30 +0000

    Fix button functionality and styling when clicking on other buttons.

--- public/js/snap.svg-createBtn.js
+++ public/js/snap.svg-createBtn.js
@@ -1,5 +1,5 @@
 
-function createBtn(name, title, iconTransform, transform, pathArray, onMouseDown) {
+function createBtn(name, title, iconTransform, transform, pathArray, onMouseDown, callback) {
   //The viewport is set to 100x100 to match the toolbox width of 100
   //This means that the icon will be at it's orig size. 32x32 icons work out of the box.
   //Anything else will need to be scaled
@@ -15,7 +15,7 @@ function createBtn(name, title, iconTransform, transform, pathArray, onMouseDown
 
   symbol_icon.add(GR_paths);
 
-  var icon = toolbox.use('icon-'+name).attr({class:'line--grid'}).transform(iconTransform)
+  var icon = toolbox.use('icon-'+name).attr({class:'icon'}).transform(iconTransform)
   var icon_title = Snap.parse('<title>'+title+'</title>');
   var icon_bkg =toolbox.rect(0, 0, 50, 50, 5, 5).attr({class: "icon-button"}).append(icon_title)
 
@@ -23,23 +23,64 @@ function createBtn(name, title, iconTransform, transform, pathArray, onMouseDown
   GR_icon_button.transform(transform)
 
   GR_icon_button.mouseover(function(e){
-    icon_bkg.toggleClass('active')
+    //icon_bkg.toggleClass('active')
     icon.attr({ fill: "#FFBA49" })
   }).mouseout(function(e){
-    icon_bkg.toggleClass('active')
+    //icon_bkg.toggleClass('active')
     icon.attr({ fill: "#000" })
   }).mousedown(function(e){
-    // Add mousedown event listner to all nodes
-    var allNodes = paper.selectAll(".node")
-    allNodes.forEach(function(el) {
-      el.mousedown(function(event){
-        console.log(this)
+    //If element has active class remove
+    //remove active class from all icons
+    if(icon.hasClass('active')){
+      icon.removeClass('active')
+      icon_bkg.removeClass('active')
+      icon.selected = false
+    }else{
+      toolbox.selectAll(".icon").forEach(function(el) {
+        el.removeClass('active')
+        el.selected = false
       })
+      toolbox.selectAll(".icon-button").forEach(function(el) {
+        el.removeClass('active')
+      })
+      icon.addClass('active')
+      icon.selected = true
+      icon_bkg.addClass('active')
+    }
+
+    //icon.toggleClass('active')
+    //this.toggle = !this.toggle
+
+    //Grab set of all nodes
+    var allNodes = paper.selectAll(".node")
+
+    allNodes.forEach(function(el) {
+      el.unmousedown()
     })
-    //If a mousedown function has been added to the button, trigger it here
+
+    console.log(this)
+
+    if(icon.selected){
+      // Add mousedown event listener to all nodes
+      allNodes.forEach(function(el) {
+        el.mousedown(function(event){
+          //console.log(this)
+          //If a mousedown function has been added to the button, trigger it here
+          if(typeof callback !== 'undefined'){
+            callback(this)
+          }
+        })
+      })
+    } else{
+      allNodes.forEach(function(el) {
+        el.unmousedown()
+      })
+    }
+
     if(typeof onMouseDown !== 'undefined'){
       onMouseDown()
     }
+
   })
 
 
