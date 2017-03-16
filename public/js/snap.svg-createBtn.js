2e85f94bb7d5e95918decbd8fac1a1b0c664fabf
robert.lavender <robert.lavender@digital.hmrc.gov.uk>
2017-03-16 12:29:20 +0000

    Add ability to pass functions to buttons.

--- public/js/snap.svg-createBtn.js
+++ public/js/snap.svg-createBtn.js
@@ -1,14 +1,40 @@
 
-function createBtn(name, title, transform, path) {
+function createBtn(name, title, iconTransform, transform, pathArray, onMouseDown) {
   //The viewport is set to 100x100 to match the toolbox width of 100
   //This means that the icon will be at it's orig size. 32x32 icons work out of the box.
   //Anything else will need to be scaled
-  var symbol_icon = toolbox.symbol(0,0,100,100).attr({id:'icon-'+name, preserveAspectRatio: 'xMinYMin meet'});
-  var icon_path = toolbox.path("M27 0c2.761 0 5 2.239 5 5 0 1.126-0.372 2.164-1 3l-2 2-7-7 2-2c0.836-0.628 1.874-1 3-1zM2 23l-2 9 9-2 18.5-18.5-7-7-18.5 18.5zM22.362 11.362l-14 14-1.724-1.724 14-14 1.724 1.724z");
-  symbol_icon.add(icon_path);
-  var icon_button_title = Snap.parse('<title>title</title>');
-  var icon_button = toolbox.use('icon-'+name).attr({class:'line--grid'}).append(icon_button_title);
-  icon_button.transform(transform)
-  return icon_button
-}
+  var symbol_icon = toolbox.symbol().attr({id:'icon-'+name});
+  // Loop over the paths array and add each path to the group
+  var GR_paths = toolbox.g()
+  icon_path = []
+  function groupPaths(element, index, array) {
+    icon_path[index] = toolbox.path(element);
+    GR_paths.add(icon_path[index])
+  }
+  pathArray.forEach(groupPaths);
 
+  symbol_icon.add(GR_paths);
+
+  var icon = toolbox.use('icon-'+name).attr({class:'line--grid'}).transform(iconTransform)
+  var icon_title = Snap.parse('<title>'+title+'</title>');
+  var icon_bkg =toolbox.rect(0, 0, 50, 50, 5, 5).attr({class: "icon-button"}).append(icon_title)
+
+  var GR_icon_button = toolbox.g(icon, icon_bkg).attr({id: 'button-icon-'+name})
+  GR_icon_button.transform(transform)
+
+  GR_icon_button.mouseover(function(e){
+    icon_bkg.toggleClass('active')
+    icon.attr({ fill: "#FFBA49" })
+  }).mouseout(function(e){
+    icon_bkg.toggleClass('active')
+    icon.attr({ fill: "#000" })
+  })
+
+  if(typeof onMouseDown !== 'undefined'){
+    GR_icon_button.mousedown(function(e){
+      onMouseDown()
+    })
+  }
+
+  return GR_icon_button
+}
